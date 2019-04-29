850e5c5734f4742734e372cba0c08c98a80f9cb8
ClayM0re <fancycode@gmail.com>
2019-04-29 16:39:10 +0800

    Create palindorome.java

---
+++ palindorome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 ) return false;
+        else if (x ==0) return true;
+        else {
+            int  []arr = new int[12];
+            int arrSize=0;
+            int v = x;
+            while(v > 0) {
+                int digit = v % 10;
+                arr[arrSize++]=(digit);
+                v /= 10;
+            }
+            int i=0;
+            int j=arrSize-1;
+            boolean ret = true;
+            while(i<j){
+               if(arr[i] == arr[j]) {
+                   i++;j--;
+               } else{
+                   ret = false;
+                   break;
+               }
+            }
+            return ret;
+        }
+    }
+}
